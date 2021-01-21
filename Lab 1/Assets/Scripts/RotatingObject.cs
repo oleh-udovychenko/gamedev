05f7734a7db978d2927414efdc6eb11260c7fac5
Oleh <prostoolegoleg@gmail.com>
2021-01-21 21:04:52 +0200

    First lab

---
+++ Lab 1/Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    [SerializeField] private Transform _transformObj;
+    [SerializeField] private float _rotationSpeed;
+
+    [SerializeField] private float _timeForDirection;
+
+    private float _startDirectionTime = 0;
+    private bool direction = true;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            ChangeDirection();
+
+        if (direction)
+            _transformObj.rotation *= Quaternion.Euler(Vector3.one * _rotationSpeed * Time.deltaTime);
+        else
+            _transformObj.rotation *= Quaternion.Euler(Vector3.one * _rotationSpeed * Time.deltaTime * -1);
+
+        if (Time.time - _startDirectionTime >= _timeForDirection)
+            ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        direction = !direction;
+        _startDirectionTime = Time.time;
+    }
+}
---
+++ Lab 1/Assets/Scripts/ScaleObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleObject : MonoBehaviour
+{
+    [SerializeField] private Transform _transformObj;
+    [SerializeField] private float _scaleSpeed;
+
+    [SerializeField] private float _maxScale;
+    [SerializeField] private float _minScale;
+
+    private bool _increase = true;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            _increase = !_increase;
+        }
+
+        if (_increase)
+        {
+            _transformObj.localScale += Vector3.one * _scaleSpeed * Time.deltaTime;
+
+            if (_transformObj.localScale.x >= _maxScale)
+                _increase = false;
+        }
+        else
+        {
+            _transformObj.localScale -= Vector3.one * _scaleSpeed * Time.deltaTime;
+
+            if (_transformObj.localScale.x <= _minScale)
+                _increase = true;
+        }
+    }
+}
